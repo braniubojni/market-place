9806673c0ceff50082289db29a890ec6e1fbaa0d
braniubojni <erik95harutyunyan@gmail.com>
2023-04-21 23:56:10 +0400

    eh/feat: first commit

---
+++ mp-front/src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {} from 'react-helmet'
+
+type Props = {}
+
+const HomePage: React.FC = (props: Props) => {
+	return (
+		<h1>MAIN - MarketPlace</h1>
+	)
+}
+
+export default HomePage
